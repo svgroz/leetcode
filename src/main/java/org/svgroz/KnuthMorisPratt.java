08f571f4d2e016f98145fff92cc9c5f424758ecf
Simon Grozovsky <svgroz@outlook.com>
2023-09-23 21:36:49 +0400

    init

--- src/main/java/org/svgroz/KnuthMorisPratt.java
+++ src/main/java/org/svgroz/KnuthMorisPratt.java
@@ -1,17 +1,17 @@
 package org.svgroz;
 
 public class KnuthMorisPratt {
-    public int[] prefix(String s) {
-        int[] pi = new int[s.length()];
+    public int[] prefix(char[] s) {
+        int[] pi = new int[s.length];
 
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 1; i < s.length; i++) {
             int j = pi[i - 1];                            // берем предыдущий j
 
-            while (j > 0 && s.charAt(j) != s.charAt(i)) { // либо до первого совпадения либо начало - начало проще
+            while (j > 0 && s[j] != s[i]) {               // либо до первого совпадения либо начало - начало проще
                 j = pi[j - 1];                            //  берем предыдущий индекс и переставляемся на него
             }
 
-            if (s.charAt(j) == s.charAt(i)) {             // успешно нашли пару - j можно увеличивать
+            if (s[j] == s[i]) {                           // успешно нашли пару - j можно увеличивать
                 j++;
             }
 
@@ -19,6 +19,28 @@ public class KnuthMorisPratt {
         }
 
         return pi;
+    }
+
+    public int indexOf(String source, String pattern) {
+        char[] rawSource = source.toCharArray(), rawPattern = pattern.toCharArray();
+        int[] prefix = prefix(rawPattern);
+
+        int current = 0;
+        for (int i = 0; i < rawSource.length; i++) {
+            while (current > 0 && rawSource[i] != rawPattern[current]) {
+                current = prefix[current - 1];
+            }
+
+            if (rawSource[i] == rawPattern[current]) {
+                current++;
+            }
+
+            if (current == rawPattern.length) {
+                return i - (current - 1);
+            }
+        }
+
+        return -1;
     }
 
 }
