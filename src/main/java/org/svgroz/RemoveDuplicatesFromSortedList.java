6e4cf17a7eeeba3d9236542774190739fe592dfd
Simon Grozovsky <svgroz@outlook.com>
2023-09-24 23:14:34 +0400

    RemoveDuplicatesFromSortedList

---
+++ src/main/java/org/svgroz/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,17 @@
+package org.svgroz;
+
+import org.svgroz.structs.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
