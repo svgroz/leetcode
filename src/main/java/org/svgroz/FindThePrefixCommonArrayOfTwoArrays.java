1f3578a76ebe5fd7a8dacb0ad53372fb7f54843c
Simon Grozovsky <svgroz@outlook.com>
2023-09-26 00:33:52 +0400

    FindThePrefixCommonArrayOfTwoArrays

--- src/main/java/org/svgroz/FindThePrefixCommonArrayOfTwoArrays.java
+++ src/main/java/org/svgroz/FindThePrefixCommonArrayOfTwoArrays.java
@@ -5,19 +5,14 @@ import java.util.HashSet;
 public class FindThePrefixCommonArrayOfTwoArrays {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         int[] res = new int[A.length];
-
         HashSet<Integer> mem = new HashSet<>();
         mem.add(A[0]);
         res[0] = mem.add(B[0]) ? 0 : 1;
-
         for (int i = 1; i < res.length; i++) {
             int c = mem.add(A[i]) ? 0 : 1;
             c += mem.add(B[i]) ? 0 : 1;
-
             res[i] = c + res[i - 1];
-
         }
-
         return res;
     }
 }
